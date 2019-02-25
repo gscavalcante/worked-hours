52bc22c37740973a7166910fa6ccfcba2b225b01
Gabriel Cavalcante <gscavalcante@gmail.com>
2019-02-25 12:12:19 -0300

    Do some changes to use python3

--- report.py
+++ report.py
@@ -1,8 +1,9 @@
+import os
 import csv
 import sys
 from datetime import datetime, timedelta
 
-FILE_NAME = "worked_hours.csv"
+file_name = ""
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%H:%M"
 
@@ -16,35 +17,39 @@ def time_to_decimal(time):
 def check_day(day):
     total = .0
 
-    with open(FILE_NAME, 'rb') as worked_hours:
-        csv_reader = csv.reader(worked_hours)
-        next(csv_reader) # Jump the header
+    with open(file_name, "r", newline="") as worked_hours:
+        reader = csv.reader(worked_hours)
 
-        for row in csv_reader:
+        for row in reader:
             if len(row) == 3 and len(row[2]) == 5 and day == row[0]:
                 start_time = datetime.strptime(row[1], TIME_FORMAT)
                 end_time = datetime.strptime(row[2], TIME_FORMAT)
 
                 total += time_to_decimal(end_time - start_time)
-    if total == .0:
-        print 'There isn\'t any checkout on this day. ' \
-            'You need to finish your work hour before check the ammount.'
+
+    if total is .0:
+        print ("There isn\'t any checkout on this day.",
+            "You need to finish your work hour before check the ammount.")
     else:
-        print total
+        print(total)
 
 def init():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    global file_name
+    file_name = dir_path + "/worked_hours.csv"
+
     option = sys.argv[1]
 
-    if option == '-d' or option == '--date':
+    if option == "-d" or option == "--date":
         check_day(sys.argv[2])
-    elif option == 'today':
+    elif option == "today":
         today_date = datetime.now()
         check_day(today_date.strftime(DATE_FORMAT))
-    elif option == 'yesterday':
+    elif option == "yesterday":
         yesterday_date = datetime.now() - timedelta(1)
         check_day(yesterday_date.strftime(DATE_FORMAT))
     else:
-        print 'Error: Unknown option %s' % option
+        print("Error: Unknown option {}".format(option))
         exit(1)
 
 init()
